649041fd23fb5dff03f4134bda90d0ffc326a2d3
Florian REBOUL <Florian REBOUL>
2017-05-17 23:34:09 +0200

    connexion

--- meetunit_front/src/client/app/users/utilisateur.ts
+++ meetunit_front/src/client/app/users/utilisateur.ts
@@ -8,9 +8,14 @@ namespace app.users {
         pseudo:string;
 
         /* @ngInject */
-        constructor(id:number, pseudo:string) {
+        constructor(id:number, pseudo:string, name:string, firstname:string, mail:string, pass:string, passV:string) {
             this.id=id;
             this.pseudo=pseudo;
+            this.name=name;
+            this.firstname=firstname;
+            this.mail=mail;
+            this.pass=pass;
+            this.passV=passV;
         }
 
     }
